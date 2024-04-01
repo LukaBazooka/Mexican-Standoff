80473c95bff3a0b2fed3c0739cb95fa22501bd7f
LukaBazooka <102615598+LukaBazooka@users.noreply.github.com>
2024-03-31 22:26:05 -0400

    updated menu

--- Menu.gd
+++ Menu.gd
@@ -7,7 +7,7 @@ func _on_options_pressed():
 	get_tree().change_scene_to_file("res://options_menu.tscn")
 
 func _on_instructions_pressed():
-	get_tree().change_scene_to_file("res://game_rules.tscn")
+	get_tree().change_scene_to_file("res://Move_List.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
